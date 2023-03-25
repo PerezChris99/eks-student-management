6a44c215538079979da83827d9e2edc0179a6953
perezchris3599 <kweeziperez712@gmail.com>
2023-03-25 13:06:52 +0300

    title

--- student.py
+++ student.py
@@ -3,8 +3,11 @@ from tkinter import *
 class Student:
     def __init__(self,root):
         self.root=root
-        self.root.title("EKS School Managenent")
+        self.root.title("EKS Student Managenent")
         self.root.geometry("1350x700+0+0")
+
+        title=Label(self.root,text="EKS Student Managenent", font=("times new roman",40,"bold"),bg="yellow",fg="red")
+        title.pack(side=TOP)
 
 
 root=Tk()
