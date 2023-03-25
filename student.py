f18b8a3a7ab369859dc03604c2508389ebf2bba4
perezchris3599 <kweeziperez712@gmail.com>
2023-03-25 13:27:16 +0300

    two crimson boxes

--- student.py
+++ student.py
@@ -6,8 +6,17 @@ class Student:
         self.root.title("EKS Student Managenent")
         self.root.geometry("1350x700+0+0")
 
-        title=Label(self.root,text="EKS Student Managenent", font=("times new roman",40,"bold"),bg="yellow",fg="red")
-        title.pack(side=TOP)
+        title=Label(self.root,text="EKS Student Managenent",bd=10,relief=GROOVE,font=("times new roman",40,"bold"),bg="yellow",fg="red")
+        title.pack(side=TOP,fill=X)
+
+        Manage_Frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+        Manage_Frame.place(x=20,y=100,width=450,height=560)
+
+        Detail_Frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+        Detail_Frame.place(x=500,y=100,width=750,height=560)
+
+
+
 
 
 root=Tk()
